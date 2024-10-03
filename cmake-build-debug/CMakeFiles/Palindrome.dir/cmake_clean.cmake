15676817ec563899527df4b77cca80da0d6f12ea
pranaykumar2 <211fa19002@vignan.ac.in>
2024-10-04 00:24:43 +0530

    Recursion Update

---
+++ cmake-build-debug/CMakeFiles/Palindrome.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Palindrome.dir/DSA/Recursion/Palindrome.c.obj"
+  "CMakeFiles/Palindrome.dir/DSA/Recursion/Palindrome.c.obj.d"
+  "Palindrome.exe"
+  "Palindrome.exe.manifest"
+  "Palindrome.pdb"
+  "libPalindrome.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Palindrome.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Palindrome.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Palindrome.
+# This may be replaced when dependencies are built.

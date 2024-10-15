6d6f8de6c05f3d9783b75730c7aff1e54945c88d
pranaykumar2 <211fa19002@vignan.ac.in>
2024-10-15 10:29:47 +0530

    IBM Software Developer Updated

--- IBM/LatestRequests.c
+++ IBM/LatestRequests.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-bool isLatest(char request[], char requests[][32], int count) {
+#define BITS 32
+
+bool isLatest(char request[], char requests[][BITS], int count) {
     for (int i = 0; i < count; i++) {
         if (strcmp(requests[i], request) == 0) {
             return false;
@@ -10,8 +12,8 @@ bool isLatest(char request[], char requests[][32], int count) {
     return true;
 }
 
-void latestRequests(char str[][32], int n) {
-    char requests[n][32];
+void latestRequests(char str[][BITS], int n) {
+    char requests[n][BITS];
     int count = 0;
     for (int i = n - 1; i >= 0; i--) {
         if (isLatest(str[i], requests, count)) {
@@ -29,10 +31,12 @@ int main() {
     int n;
     printf("Enter the number of requests: ");
     scanf("%d", &n);
-    char str[n][32];
+    getchar();
+    char str[n][BITS];
     printf("Enter the requests:\n");
     for (int i = 0; i < n; i++) {
-        scanf("%s", str[i]);
+        fgets(str[i], BITS, stdin);
+        str[i][strcspn(str[i], "\n")] = '\0';
     }
     latestRequests(str, n);
     return 0;

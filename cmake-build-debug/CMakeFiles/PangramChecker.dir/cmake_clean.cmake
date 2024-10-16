6d8744eb3ddddc6ba036c38969abae47904c56d5
pranaykumar2 <211fa19002@vignan.ac.in>
2024-10-16 15:41:09 +0530

    IBM Software Developer Updated

---
+++ cmake-build-debug/CMakeFiles/PangramChecker.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PangramChecker.dir/IBM/PangramChecker.c.obj"
+  "CMakeFiles/PangramChecker.dir/IBM/PangramChecker.c.obj.d"
+  "PangramChecker.exe"
+  "PangramChecker.exe.manifest"
+  "PangramChecker.pdb"
+  "libPangramChecker.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PangramChecker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

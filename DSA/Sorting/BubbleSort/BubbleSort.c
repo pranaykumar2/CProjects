246ec37324767674b7dd63b3e0f22ba79647ff36
pranaykumar2 <211fa19002@vignan.ac.in>
2024-10-21 17:39:39 +0530

    Bubble Sort and misc

--- DSA/Sorting/BubbleSort/BubbleSort.c
+++ DSA/Sorting/BubbleSort/BubbleSort.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "BubbleSort.h"
 
+int SWAPPED = 0;
 void readArray(int arr[], const int size) {
     for (int i = 0; i < size; i++) {
         scanf("%d", &arr[i]);
@@ -21,7 +22,11 @@ void BubbleSort(int arr[], const int size) {
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
+                SWAPPED = 1;
             }
+        }
+        if (SWAPPED == 0) {
+            break;
         }
     }
 }
---
+++ cmake-build-debug/CMakeFiles/BubbleSortExec.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for BubbleSortExec.
+# This may be replaced when dependencies are built.
